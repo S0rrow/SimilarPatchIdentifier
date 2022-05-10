c96004854b908150859a0d5211a5083d30d84fe1
S0rrow <jae9hell@gmail.com>
2022-05-10 16:59:01 +0900

    - submodule added in fv4202

--- kdt_ae.py
+++ kdt_ae.py
@@ -6,9 +6,9 @@ from keras.models import load_model
 from keras.preprocessing.sequence import pad_sequences
 import logging
 import numpy as np
-import os #nope
+import os
 import pandas as pd
-import pickle #nope
+import pickle
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.neighbors import KDTree
 from sklearn.preprocessing import MinMaxScaler
@@ -24,8 +24,32 @@ logging.basicConfig(
     level=logging.INFO
 )
 
-def log(s):
-    print(s)
+
+def is_number(s):
+    try:
+        float(s)
+        return True
+    except ValueError:
+        return False
+
+
+def del_index_num(s):
+    temp = ''
+    is_passed = False
+    for c in reversed(s):
+        if is_number(c) and not is_passed:
+            continue
+        else:
+            temp += c
+            is_passed = True
+
+    temp = list(temp)
+    temp.reverse()
+    s = ''.join(temp)
+
+    return s
+
+
     
 # AllChangeCollector --> Vector CSV(train pool) --> AE(dimension reduction on train pool) --> KDTree(effective search) --> KNN algorithm
 # project --> AllChangeCollector --> Vector --> KDTree --> Model k nearest neighbor distance #1 ~ #10
