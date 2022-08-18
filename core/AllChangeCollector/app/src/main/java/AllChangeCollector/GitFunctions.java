1a9a4231d7db997e2b88685269cb6fee39c5316c
S0rrow <jae9hell@gmail.com>
2022-08-18 16:43:44 +0900

    applied toy project changes on ACC

--- core/AllChangeCollector/app/src/main/java/AllChangeCollector/GitFunctions.java
+++ core/AllChangeCollector/app/src/main/java/AllChangeCollector/GitFunctions.java
@@ -1,248 +1,149 @@
 package AllChangeCollector;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collection;
-
 import org.eclipse.jgit.api.Git;
-import org.eclipse.jgit.api.errors.GitAPIException;
-import org.eclipse.jgit.api.errors.InvalidRemoteException;
-import org.eclipse.jgit.api.errors.TransportException;
-import org.eclipse.jgit.internal.storage.file.FileRepository;
-import org.eclipse.jgit.lib.Ref;
-import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.diff.DiffEntry;
 import org.eclipse.jgit.lib.TextProgressMonitor;
-import org.eclipse.jgit.revwalk.RevCommit;
-import org.eclipse.jgit.revwalk.RevWalk;
+import org.eclipse.jgit.revwalk.*;
+import org.eclipse.jgit.treewalk.*;
+
+import java.io.*;
+import java.util.*;
+
+import org.eclipse.jgit.internal.storage.file.FileRepository;
+import org.eclipse.jgit.lib.*;
 
 public class GitFunctions {
-    // running git clone using Runtime
-    public void clone_repo(ArrayList<String> repo_url) throws IOException, GitAPIException {
-        System.out.println("======    Starting Task : Cloning Repository    ========");
-        ProcessBuilder processBuilder = new ProcessBuilder();
+    public String name;
 
-        File directory = new File(System.getProperty("user.dir") + "/data");
-
-        if (!directory.exists()) {
-            directory.mkdir();
-        }
-
-        processBuilder.directory(directory);
-
-        for (String curr_url : repo_url) {
-            System.out.println("Cloning from " + curr_url);
-            /*
-             * processBuilder.command("git", "clone", curr_url);
-             * try {
-             * System.out.println("Start cloning " + curr_url + ".............");
-             * Process process = processBuilder.start();
-             * //process = Runtime.getRuntime().exec("git clone " + curr_url);
-             * printResult(process);
-             * } catch (IOException e) {
-             * System.out.println("Failed Cloning");
-             * e.printStackTrace();
-             * }
-             */
-        }
-
-        System.out.println("Cloning Completed\n\n");
-    }
-
-    // Cloning Git repo using JGit
-    public void clone_repo_jgit(ArrayList<String> repo_url, ArrayList<String> repo_list)
-            throws IOException, GitAPIException {
-
-        for (String curr : repo_url) {
-            // make directory
-            int start = curr.lastIndexOf("/");
-            int end = curr.lastIndexOf(".");
-            String name = curr.substring(start, end);
-
-            File curr_directory = new File(System.getProperty("user.dir") + "/data" + name);
-
-            if (!curr_directory.exists()) {
-                curr_directory.mkdir();
-                // cloning repository
-                System.out.println("Cloning from " + curr);
-                try (Git result = Git.cloneRepository()
-                        .setURI(curr)
-                        .setDirectory(curr_directory)
-                        .setProgressMonitor(new TextProgressMonitor()) // SimpleProgressMonitor -> TextProgressMonitor
-                        .call()) {
-                    System.out.println("Having repository: " + result.getRepository().getDirectory());
-                    String output = result.getRepository().getDirectory().toString();
-                    repo_list.add(output);
-                }
-            }
-        }
-    }
-
-    public String clone_designated_lcs(String url_lcs, String lcs_name)
-            throws InvalidRemoteException, TransportException, GitAPIException {
-        File curr_directory = new File(System.getProperty("user.dir") + "/lce/" + lcs_name);
-
-        if (!curr_directory.exists()) {
-            curr_directory.mkdir();
-
-            // cloning repository
-            System.out.println("Cloning from " + url_lcs);
-            try (Git result = Git.cloneRepository()
-                    .setURI(url_lcs)
-                    .setDirectory(curr_directory)
+    public boolean clone(String url, String path) {
+        String repo_name = get_repo_name_from_url(url);
+        App.logger.info("> cloning " + App.ANSI_BLUE + url + App.ANSI_RESET + " to " + App.ANSI_BLUE + path
+                + App.ANSI_RESET + " as " + App.ANSI_YELLOW + repo_name + App.ANSI_RESET);
+        try {
+            if (new File(path + "/" + repo_name).exists())
+                return true;
+            Git.cloneRepository()
+                    .setURI(url)
+                    .setDirectory(new java.io.File(path + "/" + repo_name))
                     .setProgressMonitor(new TextProgressMonitor())
-                    .call()) {
-                System.out.println("LCE repository: " + result.getRepository().getDirectory());
-                String output = result.getRepository().getDirectory().toString();
-                return output;
-            }
+                    .call();
+            return true;
+        } catch (Exception e) {
+            App.logger.error(App.ANSI_RED + "[error] > Exception : " + e.getMessage());
+            return false;
         }
-        return "error";
     }
 
-    public void crawl_commit_id(ArrayList<String> repo_name) {
-        System.out.println("===== Starting Task : Commit ID Collecting ======");
-
-        ProcessBuilder processbuilder = new ProcessBuilder();
-        processbuilder.command("git", "log", "--pretty=format:\"%H\"");
-
-        // get history commit ids using 'git log'
-        System.out.println("git log executing");
-        for (String curr_repo : repo_name) {
-            String work_dir = System.getProperty("user.dir") + "/data/" + curr_repo;
-            processbuilder.directory(new File(work_dir));
-            try {
-                Process process = processbuilder.start();
-                saveResult(process, work_dir);
-                System.out.println("commit ID for " + curr_repo + " have been extracted");
-            } catch (IOException e) {
-                e.printStackTrace();
+    private ArrayList<String> get_commit_hash(String repo_path, String file_name) {
+        ArrayList<String> hashes = new ArrayList<>();
+        try {
+            ProcessBuilder pb = new ProcessBuilder();
+            pb.directory(new File(repo_path));
+            pb.command("git", "log", "--pretty=format:%H", file_name);
+            Process process = pb.start();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            String line = "";
+            while ((line = reader.readLine()) != null) {
+                hashes.add(line);
             }
+            process.waitFor();
+            return hashes;
+        } catch (Exception e) {
+            App.logger.error(App.ANSI_RED + "[error] > Exception : " + e.getMessage());
+            return null;
         }
-        System.out.println("All commit ID extracted\n\n");
     }
 
-    // UNUSED
-    public void ranged_commit(ArrayList<String> repo_list, ArrayList<String> repo_name)
-            throws IOException, GitAPIException {
-        Gumtree gumtree = new Gumtree();
-        System.out.println("===== Starting Task : Commit ID Collecting(ranged commit) ======");
-        int i = 0; // to keep the connection between repo_list and repo_name
-        for (String name : repo_list) {
-            try (Repository repo = new FileRepository(name)) {
-                // get number of commits
+    public String[] extract_diff(String repo_git, String repo_name, String file_name, String new_cid) {
+        App.logger.trace(App.ANSI_BLUE + "[status] > extracting diff from " + repo_name + App.ANSI_RESET + " to "
+                + App.ANSI_BLUE + file_name + App.ANSI_RESET + " with " + App.ANSI_BLUE + new_cid + App.ANSI_RESET);
+        String old_cid = "";
+        boolean found = false;
+        try {
+            ArrayList<String> commit_hashes = get_commit_hash(repo_git, file_name);
+            if (commit_hashes == null) {
+                App.logger.error(App.ANSI_RED + "[error] > Failed to get commit hashes" + App.ANSI_RESET);
+                return null;
+            }
+            for (String cid : commit_hashes) {
+                if (found) {
+                    old_cid = cid;
+                    break;
+                }
+                if (cid.equals(new_cid)) {
+                    found = true;
+                }
+            }
+            if (!found) {
+                App.logger.error(App.ANSI_RED + "[error] > Failed to find commit " + new_cid + App.ANSI_RESET);
+                return null;
+            }
+        } catch (Exception e) {
+            App.logger.error(App.ANSI_RED + e.getMessage() + App.ANSI_RESET);
+            return null;
+        }
+        return extract_diff(repo_git, repo_name, file_name, new_cid, old_cid);
+    }
 
-                // get a list of all known heads, tags, remotes, ...
-                Collection<Ref> allRefs = repo.getAllRefs().values();
-
-                // a RevWalk allows to walk over commits based on some filtering that is defined
-                try (RevWalk revWalk = new RevWalk(repo)) {
-                    int count = 0;
-                    for (Ref ref : allRefs) {
-                        revWalk.markStart(revWalk.parseCommit(ref.getObjectId()));
-                    }
-                    ArrayList<String> commit_sha_list = new ArrayList<String>();
-                    for (RevCommit commit : revWalk) {
-                        String beforeCommit = commit.getName();
-                        commit_sha_list.add(beforeCommit);
-                        count++;
-                    }
-
-                    // for size of the commit, sets range of the collecting commits
-                    int start;
-                    int end;
-                    if (count < 1000) {
-                        start = 0;
-                        end = count;
-                    } else if (count >= 1000 && count < 10000) {
-                        start = (int) (count * 0.8);
-                        end = (int) (count * 0.9);
-
+    public String[] extract_diff(String repo_git, String repo_name, String file_name, String new_cid, String old_cid) {
+        App.logger.trace(App.ANSI_BLUE + "[status] > extracting diff from " + App.ANSI_BLUE + repo_name + App.ANSI_RESET
+                + " between "
+                + App.ANSI_BLUE + old_cid + App.ANSI_RESET + " and " + App.ANSI_BLUE + new_cid + App.ANSI_RESET);
+        String[] result = new String[4];
+        try {
+            Repository repo = new FileRepository(repo_git);
+            App.logger.info(App.ANSI_YELLOW + "[status] > repo " + repo_name + App.ANSI_RESET + " is "
+                    + App.ANSI_YELLOW + repo.getDirectory().getAbsolutePath() + App.ANSI_RESET);
+            ObjectId oldHead = repo.resolve(old_cid + "^{tree}");
+            ObjectId newHead = repo.resolve(new_cid + "^{tree}");
+            if (oldHead == null || newHead == null) {
+                App.logger.error(App.ANSI_RED + "[error] > oldHead or newHead is null" + App.ANSI_RESET);
+                repo.close();
+                return null;
+            }
+            ObjectReader reader = repo.newObjectReader();
+            CanonicalTreeParser oldTreeIter = new CanonicalTreeParser();
+            oldTreeIter.reset(reader, oldHead);
+            CanonicalTreeParser newTreeIter = new CanonicalTreeParser();
+            newTreeIter.reset(reader, newHead);
+            Git git = new Git(repo);
+            List<DiffEntry> diffs = git.diff().setNewTree(newTreeIter).setOldTree(oldTreeIter).call();
+            for (DiffEntry entry : diffs) {
+                String str_new = entry.getNewPath();
+                String str_old = entry.getOldPath();
+                if (str_new.endsWith(".java") && str_old.endsWith(".java")) {
+                    if (file_name.equals("")) {
+                        result[0] = new_cid;
+                        result[1] = old_cid;
+                        result[2] = str_new;
+                        result[3] = str_old;
                     } else {
-                        start = (int) (count * 0.7);
-                        end = start + 1000;
-                    }
-                    // execution
-                    for (int j = start; j < end; j++) {
-                        if (j - 1 > 0) {
-                            gumtree.get_changed_file(repo_list.get(i), repo_name.get(i), commit_sha_list.get(j),
-                                    commit_sha_list.get(j - 1)); // (repo_git_directory, repo_name, current commit, old
-                                                                 // commit)
+                        if (str_new.contains(file_name)) {
+                            result[0] = new_cid;
+                            result[1] = old_cid;
+                            result[2] = str_new;
+                            result[3] = str_old;
                         }
                     }
-
                 }
-                i++;
             }
-            // reader.close();
-            // writer.close();
-            System.out.println();
+            git.close();
+            repo.close();
+        } catch (Exception e) {
+            App.logger.error(App.ANSI_RED + "[error] > Exception : " + e.getMessage() + App.ANSI_RESET);
+            return null;
         }
+        return result;
     }
 
-    public void all_commit(ArrayList<String> repo_list, ArrayList<String> repo_name)
-            throws IOException, GitAPIException {
-        Gumtree gumtree = new Gumtree();
-        System.out.println("====== Starting Task : Commit ID Collecting(all commit) ======");
-        int i = 0; // to keep the connection between repo_list and repo_name
-        for (String name : repo_list) {
-            try (Repository repo = new FileRepository(name)) {
-
-                // get a list of all known heads, tags, remotes, ...
-                Collection<Ref> allRefs = repo.getRefDatabase().getRefs();
-
-                // a RevWalk allows to walk over commits based on some filtering that is defined
-                try (RevWalk revWalk = new RevWalk(repo)) {
-                    for (Ref ref : allRefs) {
-                        revWalk.markStart(revWalk.parseCommit(ref.getObjectId()));
-                    }
-                    int count = 0;
-                    ArrayList<String> commit_sha_list = new ArrayList<String>();
-                    for (RevCommit commit : revWalk) {
-                        String beforeCommit = commit.getName();
-                        if (commit_sha_list.size() == 0) {
-                            commit_sha_list.add(beforeCommit);
-                            count++;
-                        } else {
-                            gumtree.get_changed_file(repo_list.get(i), repo_name.get(i), commit_sha_list.get(count - 1),
-                                    beforeCommit); // (repo_git_directory, repo_name, current commit, old commit)
-                            gumtree.get_log(repo_list.get(i), repo_name.get(i),
-                                    commit_sha_list.get(count - 1), beforeCommit);
-                            commit_sha_list.add(beforeCommit);
-                            count++;
-                        }
-
-                    }
-                }
-                i++;
+    // use globally for extracting repo name from url
+    public static String get_repo_name_from_url(String url) {
+        String[] url_split = url.split("/");
+        for (String split : url_split) {
+            if (split.contains(".git")) {
+                return split.replace(".git", "");
             }
         }
-    }
-
-    public void printResult(Process process) throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
-        String line = "";
-        while ((line = reader.readLine()) != null) {
-            System.out.println(line);
-        }
-        reader.close();
-    }
-
-    // saves all the commitID in the filename 'commitID.txt'
-    public void saveResult(Process process, String working_dir) throws IOException {
-        File file = new File(working_dir, "commitID.txt");
-        BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
-        BufferedWriter writer = new BufferedWriter(new FileWriter(file));
-        String line = "";
-        while ((line = reader.readLine()) != null) {
-            line = line.substring(1, line.length() - 1);
-            writer.write(line + "\n");
-        }
-        writer.close();
-        reader.close();
+        return url_split[url_split.length - 1];
     }
 }
