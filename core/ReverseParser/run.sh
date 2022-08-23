23c02d00272ad908f93110ad25a9c360058de4e9
Turbstructor <ycw0201@naver.com>
2022-08-23 16:31:57 +0900

    Removed unnecessary scripts

--- core/ReverseParser/run.sh
+++
@@ -1,9 +0,0 @@
-# $1 = before CID
-# $2 = git directory
-# $3 = target directory
-# $4 = file path
-# python3 reverse_parser.py -c $1 -g $2 -t $3 -f $4
-python3 reverse_parser.py -c e950938f171b3cd6367e7e6d7b1d252e6f0e8c9b -g https://github.com/apache/commons-jcs.git -t target -f src/test/org/apache/jcs/access/TestCacheAccess.java
-# result = afterCID.txt
-
-#./run.sh e950938f171b3cd6367e7e6d7b1d252e6f0e8c9b https://github.com/apache/commons-jcs.git target src/test/org/apache/jcs/access/TestCacheAccess.java
