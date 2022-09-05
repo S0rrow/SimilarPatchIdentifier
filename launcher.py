044fe3bd048f6f27f1d0e7712c807a572b63dcff
Turbstructor <ycw0201@naver.com>
2022-09-05 22:00:15 +0900

    launcher: modularization on progress

--- launcher.py
+++ launcher.py
@@ -1,10 +1,9 @@
 import argparse
 import configparser
-
+import shutil
 import sys
 import os
-from shutil import rmtree
-
+import zipfile
 import datetime as dt
 # import pandas as pd
 
@@ -15,6 +14,8 @@ import subprocess
         - Add '--rebuild' option. If enabled, SPI.py will rebuild all SPI submodules.
         - Config .properties files here. DO NOT additionally make shell scripts.
 '''
+
+cases, settings = list(), dict()
 
 def parse_argv():
     parser = argparse.ArgumentParser()
@@ -37,8 +38,7 @@ def parse_argv():
     args = parser.parse_args()
 
     cases = list()
-    case = dict()
-    settings = dict()
+    global cases, settings
     if args.debug == True:
         cases.append(dict())
         cases[-1]['mode'] = 'defects4j'
@@ -72,50 +72,117 @@ def parse_argv():
     settings['quiet'] = False if args.verbose else args.quiet # suppresses quiet option if verbose option is given
     settings['rebuild'] = args.rebuild
 
-    return cases, settings # return target data
+#######
+# Misc
+#######
 
-def rebuild(module_name : str) -> bool:
+def rebuild(module_name : str, root) -> bool:
+    #print(f"> Rebuilding {module_name}...")
     try:
-        assert subprocess.run(("gradle", "distZip", "-q"), cwd = f"./core/{module_name}")
-        assert subprocess.run(("mv", "app/build/distributions/app.zip", "../../pkg/"), cwd = f"./core/{module_name}")
+        assert subprocess.run(("gradle", "distZip", "-q"), cwd = f"./core/{module_name}", shell=True)
+        #assert subprocess.run(("mv", "app/build/distributions/app.zip", "../../pkg/"), cwd = f"./core/{module_name}", shell=True)
+        #print(f"> moving app.zip to pkg...")
+        if not move(f"./core/{module_name}/app/build/distributions/app.zip", f"{root}/pkg/", copy_function = shutil.copy):
+            print(f"> Error occurred while moving app.zip to pkg.")
+            return False
+        #print(f"> unzipping {module_name}...")
+        #assert subprocess.run(("unzip", "-q", "app.zip"), cwd = "./pkg", shell=True)
+        if not unzip("./pkg/app.zip", "./pkg/"):
+            print(f"> Error occurred while unzipping {module_name}.")
+            return False
+        #print(f"> creating {module_name} directory...")
+        if not os.path.exists(f"./pkg/{module_name}"):
+            assert subprocess.run(("mkdir", f"{module_name}"), cwd = "./pkg", shell=True)
+        #assert subprocess.run(("mv", "app", module_name), cwd = "./pkg", shell=True)
+        #print(f"> moving app to {module_name}...")
+        if not move("./pkg/app", f"./pkg/{module_name}", shutil.copy2):
+            print(f"> Error occurred while moving app to {module_name}.")
+            return False
 
-        assert subprocess.run(("unzip", "-q", "app.zip"), cwd = "./pkg")
-        assert subprocess.run(("mv", "app", module_name), cwd = "./pkg")
+        #assert subprocess.run(("rm", "app.zip"), cwd = "./pkg", shell=True)
+        #print(f"> removing app.zip...")
+        if not remove("./pkg/app.zip"):
+            print(f"> Error occurred while removing app.zip.")
+            return False
 
-        assert subprocess.run(("rm", "app.zip"), cwd = "./pkg")
-
-    except AssertionError as e:
-        print(f"Error occurred while rebuilding submodule.")
+    except Exception as e:
+        print(f"> Error occurred while rebuilding submodule {module_name}.")
+        print(f"> Error : {e}")
         return False
-    
+    #print(f"> Done.")
     return True
 
-def rebuild_confix() -> bool:
+def rebuild_confix(root) -> bool:
     # os.system(f"cd {root}/core/confix/ConFix-code ;"
     #         + "mvn clean package ;"
     #         + f"cp target/confix-0.0.1-SNAPSHOT-jar-with-dependencies.jar {root}/core/confix/lib/confix-ami_torun.jar")
     try:
-        assert subprocess.run(("mvn", "clean", "package", "-q"), cwd = "./core/confix/ConFix-code")
-        assert subprocess.run(("cp", "target/confix-0.0.1-SNAPSHOT-jar-with-dependencies.jar", "../lib/confix-ami_torun.jar"), cwd = "./core/confix/ConFix-code")
+        assert subprocess.run(("mvn", "clean", "package", "-q"), cwd = "./core/confix/ConFix-code", shell=True)
+        #assert subprocess.run(("cp", "target/confix-0.0.1-SNAPSHOT-jar-with-dependencies.jar", "../lib/confix-ami_torun.jar"), cwd = "./core/confix/ConFix-code", shell=True)
+        if not (copy(f"{root}/core/confix/ConFix-code/target/confix-0.0.1-SNAPSHOT-jar-with-dependencies.jar", f"{root}/core/confix/lib/confix-ami_torun.jar")):
+            print("Error occurred while copying ConFix jar file.")
+            return False
 
     except AssertionError as e:
         print("> ! Error occurred while rebuilding ConFix.")
-        print()
+        print("> ! Error : ", e)
         return False
 
     return True
 
-def rebuild_all():
+def move(location, destination, copy_function) -> bool:
     try:
-        if os.path.isdir("pkg"):
-            rmtree("pkg")
-        os.mkdir("pkg")
+        shutil.move(location, destination, copy_function)
+    except Exception as e:
+        print(f"> Error: {location} : {e.strerror}")
+        print(f"> ! Error occurred while moving {location} to {destination}.")
+        return False
+    return True
 
+def unzip(file, destination):
+    try:
+        with zipfile.ZipFile(file, 'r') as zip_ref:
+            zip_ref.extractall(destination)
+    except Exception as e:
+        print(f"> Error: {file} : {e.strerror}")
+        print(f"> ! Error occurred while unzipping {file}.")
+        return False
+    return True
+
+def copy(file, destination):
+    try:
+        shutil.copy(file, destination)
+    except Exception as e:
+        print(f"> Error: {file} : {e.strerror}")
+        print(f"> ! Error occurred while copying {file} to {destination}.")
+        return False
+    return True
+
+def remove(path):
+    try:
+        if(os.path.exists(path)):
+            if(os.path.isdir(path)):
+                shutil.rmtree(path)
+            else:
+                os.remove(path)
+    except Exception as e:
+        print(f"> Error: {path} : {e.strerror}")
+        print(f"> ! Error occurred while removing {path}.")
+        return False
+    return True
+
+def rebuild_all(root):
+    try:
+        #assert subprocess.run(("rm", "-rf", "pkg"))
+        if not (remove(f"{root}/pkg")):
+            print(f"> ! Error occurred while removing {root}/pkg.")
+        assert subprocess.run(("mkdir", "pkg"), shell=True)
+        #os.mkdir(f"{root}/pkg")
         for submodule in ("BuggyChangeCollector", "AllChangeCollector", "LCE"):
-            assert rebuild(submodule)
+            assert rebuild(submodule, root)
             print(f"> Successfully rebuilt submodule {submodule}.")
 
-        assert rebuild_confix() # ConFix uses maven unlike any other packages; this should be handled differently.
+        assert rebuild_confix(root) # ConFix uses maven unlike any other packages; this should be handled differently.
         print(f"> Successfully rebuilt submodule ConFix.")
         print()
 
@@ -124,7 +191,7 @@ def rebuild_all():
         print()
         return False
     except Exception as e:
-        print("> ! Error occurred: directory 'pkg' cannot be removed.")
+        print(f"> ! Error occurred while rebuilding modules: {e}")
         print()
         return False
 
@@ -133,16 +200,37 @@ def rebuild_all():
     return True
 
 
+#######
+# Module launcher
+#######
+
+def run_BCC() -> bool:
+
+    return True
+
+def run_ACC() -> bool:
+    return True
+
+def run_LCE() -> bool:
+    return True
+
+def run_ConFix() -> bool:
+    return True
+
+#######
+# Main
+#######
 
 def main(argv):
-    cases, settings = parse_argv()
+    parse_argv()
+
+    root = os.getcwd()
+    SPI_core_directory = f"{root}/core"
+    target_dir = f"{root}/target"
 
     if settings["rebuild"]:
         print("Have been requested to rebuild all submodules. Commencing...")
-        rebuild_all()
-
-    # print(case)
-    # print(settings)
+        rebuild_all(root)
 
     if settings["mode"] is None:
         print("You have not told me what to fix. Exiting program.")
@@ -161,12 +249,6 @@ def main(argv):
 
     success = 0
     fail = 0
-
-    step = 0
-
-    root = os.getcwd()
-    SPI_core_directory = f"{root}/core"
-    target_dir = f"{root}/target"
 
     whole_start = dt.datetime.now()
 
@@ -196,118 +278,14 @@ def main(argv):
         step = 0
         
         try:
-            assert subprocess.run(("cp", "SPI.properties", f"{target_dir}/"))
-
-            # Commit Collector
-            print("||| Step 1. Launching Commit Collector...")
-            start = dt.datetime.now()
-            # Commence
-
-            if settings["mode"] == "github":
-                pass
-                # executing_command = f"{root}/pkg/BuggyChangeCollector/bin/app -h {case['hash_id']} --githubinput {case['project_name']},{case['faulty_file']},{case['faulty_line']},{case['commit_id']},{case['repository']}"
-            else:
-                executing_command = f"{root}/pkg/BuggyChangeCollector/bin/app -h {case['hash_id']} --defects4j {case['project_name']} --config {target_dir}/SPI.properties"
-            print(executing_command)
-            assert subprocess.run(executing_command, shell = True).returncode == 0, "Failure occurred launching Commit Collector."
-
-    #         bfic = pd.read_csv(f"{target_dir}/outputs/commit_collector/BFIC.csv", names = ['Project', 'D4J ID', 'Faulty file path', 'Faulty line', 'FIC_sha', 'BFIC_sha']).values[1]
-    #         case['buggy_file'] = f"{target_dir}/{case['identifier']}/{bfic[2]}"
-    #         buggy_file_pt1 = f"{target_dir}/{case['identifier']}"
-    #         buggy_file_pt2 = f"{bfic[2]}"
-
-
-    #         end = dt.datetime.now()
-    #         if not is_quiet:
-    #             print(f"||| Step 1 Time taken : {(end - start)}\n")
-
-    #         if is_on_TUI:
-    #             print(f"EOS1 {buggy_file_pt1} {buggy_file_pt2} {bfic[3]}")
-
-
-    #         step = 2
-            # AllChangeCollector
-            print("||| Step 2. Launching AllChangeCollector...")
-            start = dt.datetime.now()
-            # Commence
-
-            ACC_output_directory = f"{target_dir}/outputs/AllChangeCollector"
-            JAVA_11_HOME = f"/usr/lib/jvm/java-11-openjdk-amd64"
-            ACC_core_directory = f"{SPI_core_directory}/AllChangeCollector/app/build/distributions/app/bin/"
-            os.makedirs(ACC_output_directory)
-
-            with open(f"{target_dir}/collection.txt", "w") as outfile:
-                outfile.write(f"{bfic[2]} {bfic[4]} {case['project_name']} {target_dir}/{case['identifier']}\n")
-
-            assert subprocess.run(f"cd {ACC_output_directory}; JAVA_HOME={JAVA_11_HOME} {ACC_core_directory}/app -l {target_dir}/collection.txt", shell = True, stdout = out, stderr = err).returncode == 0, "Failure occurred launching AllChangeCollector."
-
-
-    #         end = dt.datetime.now()
-    #         if not is_quiet:
-    #             print(f"||| Step 2 Time taken : {(end - start)}\n")
-    #         if is_on_TUI:
-    #             print(f"EOS2")
-
-
-    #         step = 3
-    #         # Longest Common sub-vector Extractor (LCE)
-    #         if not is_quiet:
-    #             print("||| Step 3. Launching Longest Common sub-vector Extractor(LCE)...")
-    #         start = dt.datetime.now()
-    #         # Commence
-
-    #         candidate_number = 10
-    #         LCE_output_directory = f"{target_dir}/outputs/fv4202"
-
-    #         os.makedirs(LCE_output_directory)
-    #         os.makedirs(f"{LCE_output_directory}/pool")
-    #         os.makedirs(f"{LCE_output_directory}/result")
-    #         os.makedirs(f"{target_dir}/outputs/prepare_pool_source")
-
-    #         gumtree_vector = f"{root}/CSV_Combiner/result/gumtree_vector.csv"
-    #         commit_file = f"{root}/CSV_Combiner/result/commit_file.csv"
-    #         target_vector = f"{ACC_output_directory}/vector/{case['project_name']}_gumtree_vector.csv"
-    #         d4j_project_name, d4j_project_num = case['project_name'].split("-")
-    #         with open(f"{LCE_output_directory}/lce.properties", "w") as outfile:
-    #             outfile.write(f"SPI.dir={root}\npool_file.dir={gumtree_vector}\nmeta_pool_file.dir={commit_file}\ntarget_vector.dir={target_vector}\npool.dir={LCE_output_directory}/pool/\ncandidates.dir={target_dir}/outputs/prepare_pool_source/\ncandidate_number={candidate_number}\nd4j_project_name={d4j_project_name}\nd4j_project_num={d4j_project_num}\n")
-    #         assert subprocess.run(f"cd {SPI_core_directory}/LCE; gradle run -args=\"{LCE_output_directory}/lce.properties\"")
-    #         #assert subprocess.run(f"cd {SPI_core_directory}/LCE; python3 main.py -g {gumtree_vector} -c {commit_file} -t {target_vector} -r {LCE_output_directory} > {LCE_output_directory}/log.txt", shell = True, stdout = out, stderr = err).returncode == 0, "Failure occurred launching LCE part 1."
-    #         #assert subprocess.run(f"cd {SPI_core_directory}/LCE; python3 validator.py -f meta_resultPool.csv -d {LCE_output_directory}/pool -n 10 -r {LCE_output_directory} -c {target_dir}/outputs/prepare_pool_source >> {LCE_output_directory}/log.txt", shell = True, stdout = out, stderr = err).returncode == 0, "Failure occurred launching LCE part 2."
-
-
-    #         end = dt.datetime.now()
-    #         if not is_quiet:
-    #             print(f"||| Step 3 Time taken : {(end - start)}\n")            
-    #         if is_on_TUI:
-    #             print(f"EOS3 {LCE_output_directory}/similar_patch_list.txt")
-
-    #         step = 4
-    #         # ConFix
-    #         if not is_quiet:
-    #             print("||| Step 4. Launching ConFix...")
-    #         start = dt.datetime.now()
-    #         # Commence
-
-
-    #         if case['is_defects4j'] == True:
-    #             executing_command = f"python3 {SPI_core_directory}/confix/run_confix.py -d true -h {case['hash_id']}"
-    #         else:
-    #             executing_command = f"python3 {SPI_core_directory}/confix/run_confix.py -h {case['hash_id']} -i {case['source_path']},{case['target_path']},{case['test_list']},{case['test_target_path']},{case['compile_target_path']},{case['build_tool']}"
-    #         each_exit_code = subprocess.run(executing_command, shell = True, stdout = out, stderr = err).returncode
-
-
-    #         end = dt.datetime.now()
-    #         if not is_quiet:
-    #             print(f"||| Step 4 Time taken : {(end - start)}")
-    #         if is_on_TUI:
-    #             if each_exit_code == 0:
-    #                 print("EOS4 Success") # End of Step 4
-    #             else:
-    #                 print("EOS4 Failure") # End of Step 4
+            assert run_BCC() is True, "BCC launch failed"
+            assert run_ACC() is True, "ACC launch failed"
+            assert run_LCE() is True, "LCE launch failed"
+            assert run_ConFix() is True, "ConFix launch failed"
 
         except AssertionError as e:
-            each_exit_code = 1
-            print(f"!EOS")
+            print(e)
+            print("Total SPI has failed.")
 
     #     each_end = dt.datetime.now()
     #     each_elapsed_time = (each_end - each_start)
