dec3f920473d9663e5366b341b704500f7114b46
S0rrow <jae9hell@gmail.com>
2022-09-13 15:46:52 +0900

    implmental update

--- core/AllChangeCollector/app/src/main/java/AllChangeCollector/Implemental.java
+++ core/AllChangeCollector/app/src/main/java/AllChangeCollector/Implemental.java
@@ -4,10 +4,11 @@ import org.apache.logging.log4j.Logger;
 
 public class Implemental {
     public Logger logger = App.logger;
-    
+
+    public String name = null; // Defects4J Bug Name
+    public int identifier = -1; // Defects4J Bug Identifier
+
     public Implemental() {
         super();
     }
-
-    
 }
